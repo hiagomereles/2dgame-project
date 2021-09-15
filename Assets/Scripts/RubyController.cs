e27cc7285db8e9df9ef5aa871d0849e0c77ae573
hiago mereles <hiago.meirelles@outlook.com>
2021-09-15 10:55:17 -0300

    World Intereactions
    Added some accelerators to the environment and to objects and had them adjusted to fit the world correctly.

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2D;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");     
+    }
 
-        Vector2 position = transform.position;
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
         position.x = position.x + 3f * horizontal * Time.deltaTime;
         position.y = position.y + 3f * vertical * Time.deltaTime;
-        transform.position = position;
+        rigidbody2D.MovePosition(position);
     }
 }
